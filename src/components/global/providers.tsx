ac02c698b99a05858e58aae3b9acf51188bb38ee
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-01-21 13:11:06 +0530

    Provide

--- src/components/global/providers.tsx
+++ src/components/global/providers.tsx
@@ -1,23 +1,21 @@
 "use client";
 
-import React from "react"
-import { ClerkProvider } from "@clerk/nextjs"
+import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
 // import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 interface Props {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 // const client = new QueryClient();
 
 const Providers = ({ children }: Props) => {
-    return (
-        // <QueryClientProvider client={client}>
-        <ClerkProvider>
-            {children}
-        </ClerkProvider>
-        // </QueryClientProvider> 
-    );
+  return (
+    // <QueryClientProvider client={client}>
+    <ClerkProvider>{children}</ClerkProvider>
+    // </QueryClientProvider>
+  );
 };
 
-export default Providers
+export default Providers;
