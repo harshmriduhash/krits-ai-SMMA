66f1860771d0e575b65b970bd49fec15496c5c9f
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-03-11 16:21:10 +0530

    Page

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -2,15 +2,15 @@ import { SignInForm } from "@/components";
 import { Suspense } from "react";
 
 const SignInPage = () => {
-    return (
-        <div className="flex flex-col items-center justify-center size-full">
-            <div className="max-w-xs mx-auto size-full flex flex-col items-center mt-[270px]">
-                <Suspense>
-                    <SignInForm />
-                </Suspense>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center justify-center size-full">
+      <div className="max-w-xs mx-auto size-full flex flex-col items-center mt-[270px]">
+        <Suspense>
+          <SignInForm />
+        </Suspense>
+      </div>
+    </div>
+  );
 };
 
-export default SignInPage
+export default SignInPage;
